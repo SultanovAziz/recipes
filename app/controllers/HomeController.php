6df001eb8e2c2cf79c5e9193b41a9276fe88f1f7
Admin <aziz89131983189@gmail.com>
2022-03-18 21:09:11 +0700

    add base view

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -1,1 +1,1 @@
-<?phpnamespace app\controllers;class HomeController extends AppController{    public function __construct($route)    {        parent::__construct($route);    }    public function indexAction()    {        echo 'ok';    }}
+<?phpnamespace app\controllers;class HomeController extends AppController{    public function __construct($route)    {        parent::__construct($route);    }    public function indexAction()    {        $this->setMeta('Главная страница','Описание','Ключевики');    }}
